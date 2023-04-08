0765fc7fa5334f4536ba6abcfc5fb3958123f9cf
GutowskiO <127432389+GutowskiO@users.noreply.github.com>
2023-04-08 12:46:17 +0200

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;    
+
+public class Swap {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = new int[5];
+        array[0] = 1;
+        array[1] = 3;
+        array[2] = 5;
+        array[3] = 7;
+        array[4] = 9;
+
+        int index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+        System.out.println("");
+
+        System.out.println("What indicies would you like to swap?");
+        int first = Integer.valueOf(scanner.nextLine());
+        int second = Integer.valueOf(scanner.nextLine());
+
+        int container = array[first];
+        array[first] = array[second];
+        array[second] = container;
+
+        System.out.println("");
+        index = 0;
+        while (index < array.length) {
+            System.out.println(array[index]);
+            index++;
+        }
+    }
+
+}
