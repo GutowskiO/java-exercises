22bd44ce8c5b475e887edcf772c0ac625afad5de
GutowskiO <127432389+GutowskiO@users.noreply.github.com>
2023-04-21 20:13:37 +0200

    Add files via upload

---
+++ NumberOfStringsExercise/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        String[] lor = {omg, omg};
+        System.out.println(lor[1]);
+
+        while (true) {
+
+        }
+    }
+}
