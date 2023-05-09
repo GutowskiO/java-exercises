20b48e15e2ecee6af638f265c967456b9d4f968c
GutowskiO <127432389+GutowskiO@users.noreply.github.com>
2023-05-09 20:21:41 +0200

    Add files via upload

---
+++ CardPaymentsP5/src/PaymentCard.java
@@ -0,0 +1,24 @@
+public class PaymentCard {
+    private double balance;
+
+    public PaymentCard(double balance) {
+        this.balance = balance;
+    }
+
+    public double balance() {
+        return this.balance;
+    }
+
+    public void addMoney(double increase) {
+        this.balance = this.balance + increase;
+    }
+
+    public boolean takeMoney(double amount) {
+        if (amount > this.balance) {
+            return false;
+        }
+
+        this.balance -= amount;
+        return true;
+    }
+}
